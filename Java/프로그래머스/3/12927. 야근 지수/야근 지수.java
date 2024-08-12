4b1a89eab38096b19db3987c6e654d90089d745d
Changyeong Yeo <67392167+duckddud213@users.noreply.github.com>
2024-08-12 14:31:21 +0900

    [level 3] Title: 야근 지수, Time: 129.93 ms, Memory: 68 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    static int N;
+    static long answer;
+    static PriorityQueue<Integer> pq;
+    
+    public static void work(int remain[]){
+        for(int num : remain){
+            pq.add(num);
+        }
+        
+        for(int time = N; time > 0 && pq.peek() > 0; time--){
+            int num = pq.poll();
+            pq.add(num - 1);
+        }
+        
+        while(!pq.isEmpty()){
+            int num = pq.poll();
+            answer += (num * num);
+        }
+    }
+    
+    public long solution(int n, int[] works) {
+        answer = 0;
+        N = n;
+        
+        pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        work(works);
+        
+        return answer;
+    }
+}
