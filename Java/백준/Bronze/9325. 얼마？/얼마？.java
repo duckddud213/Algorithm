d14c83af835dda25f600fb85a7b73b6c41b6eb06
Changyeong Yeo <67392167+duckddud213@users.noreply.github.com>
2024-08-19 00:49:19 +0900

    [Bronze III] Title: 얼마?, Time: 248 ms, Memory: 28064 KB -BaekjoonHub

---
+++ Java/백준/Bronze/9325. 얼마？/얼마？.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int x = 1; x <= T; x++){
+            int sum = Integer.parseInt(br.readLine());
+            int option = Integer.parseInt(br.readLine());
+            for(int i = 0; i < option; i++){
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                sum += (a * b);
+            }
+            System.out.println(sum);
+//            sb.append(sum).append('\n');
+        }
+        
+//        System.out.print(sum);
+    }
+}
