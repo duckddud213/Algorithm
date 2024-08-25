0d26093f221823965d903a1b3bb15a2f6c52df21
Changyeong Yeo <67392167+duckddud213@users.noreply.github.com>
2024-08-25 19:24:19 +0900

    [Bronze IV] Title: 히스토그램, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

---
+++ Java/백준/Bronze/13752. 히스토그램/히스토그램.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        
+        for(int i = 1; i <= N; i++){
+            int cnt = Integer.parseInt(br.readLine());
+            for(int j = 1; j <= cnt; j++){
+                sb.append("=");
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+}
