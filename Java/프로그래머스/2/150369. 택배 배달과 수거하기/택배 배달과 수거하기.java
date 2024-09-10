45b2b51fb07993726cb8df84f40c9045fda3d435
Changyeong Yeo <67392167+duckddud213@users.noreply.github.com>
2024-09-10 12:12:45 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 15.72 ms, Memory: 102 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+        int deliver,pickup,check;
+        
+        deliver = 0;
+        pickup = 0;
+        
+        for(int i=n-1;i>=0;i--){
+            if(deliveries[i]!=0 || pickups[i]!=0){
+                check=0;
+                
+                while(deliver<deliveries[i] || pickup<pickups[i]){
+                    deliver+=cap;
+                    pickup+=cap;
+                    check++;
+                }
+                
+                deliver-=deliveries[i];
+                pickup-=pickups[i];
+                answer+=(i+1)*2*check;
+            }
+        }
+        
+        return answer;
+    }
+}
