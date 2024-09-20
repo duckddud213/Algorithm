f52725b34f71684bfaf4318082c2df75371ac716
Changyeong Yeo <67392167+duckddud213@users.noreply.github.com>
2024-09-21 01:41:05 +0900

    [Bronze IV] Title: 이칙연산, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

---
+++ Java/백준/Bronze/15726. 이칙연산/이칙연산.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double A = Double.parseDouble(st.nextToken());
+        double B = Double.parseDouble(st.nextToken());
+        double C = Double.parseDouble(st.nextToken());
+
+        int result = (int) (A * Math.max(B, C) / Math.min(B, C));
+
+        System.out.println(result);
+    }
+}
