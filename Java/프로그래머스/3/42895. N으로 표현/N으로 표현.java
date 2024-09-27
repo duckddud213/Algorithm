4dda30af7edd5635bc220499f1d07d776ce2c8f1
Changyeong Yeo <67392167+duckddud213@users.noreply.github.com>
2024-09-27 19:07:47 +0900

    [level 3] Title: N으로 표현, Time: 39.39 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int N, int number) {
+        List<Set<Integer>> setList = new ArrayList<>();
+        for(int i = 0; i <= 8; i++) {
+            setList.add(new HashSet<>());
+        }
+        // 1회
+        setList.get(1).add(N);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(N);
+
+        // 2회 이상
+        for(int i = 2; i < setList.size(); i++) {
+            sb.append(N);
+            Set<Integer> newSet = setList.get(i);
+            newSet.add(Integer.parseInt(sb.toString()));
+
+            for(int j = 1; j <= i; j++) {
+                Set<Integer> setA = setList.get(j);
+                Set<Integer> setB = setList.get(i - j);
+
+                for (Integer numA : setA) {
+                    for (Integer numB : setB) {
+                        newSet.add(numA + numB);
+                        newSet.add(numA - numB);
+                        newSet.add(numA * numB);
+                        if(numA != 0 && numB != 0) {
+                            newSet.add(numA / numB);
+                        }
+                    }
+                }
+            }
+        }
+        for (Set<Integer> set : setList) {
+            if(set.contains(number)) return setList.indexOf(set);
+        }
+        return -1;
+    }
+}
