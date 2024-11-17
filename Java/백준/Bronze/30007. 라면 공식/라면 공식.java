a24caece61ffcc5d57de4d386940dae23c84ff90
Changyeong Yeo <67392167+duckddud213@users.noreply.github.com>
2024-11-18 00:49:07 +0900

    [Bronze V] Title: 라면 공식, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

---
+++ Java/백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for(int i = 1; i <= N; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int w = a * (x - 1) + b;
+            sb.append(w).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
