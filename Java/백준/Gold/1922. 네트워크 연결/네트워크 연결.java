d11e8dad9da7dac7e84139b81c71826e67c74cf3
Changyeong Yeo <67392167+duckddud213@users.noreply.github.com>
2025-09-27 02:46:47 +0900

    [Gold IV] Title: 네트워크 연결, Time: 604 ms, Memory: 61936 KB -BaekjoonHub

--- Java/백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ Java/백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -2,78 +2,88 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M,distance;
-    static HashSet<Integer> isConnected;
-    static PriorityQueue<Connection> pq;
-    static List<Connection>[] conn;
+    static int N, M, sum;
+    static List<Node> graph[];
+    static PriorityQueue<Node> pq;
+    static HashSet<Integer> connected;
 
     public static void Prims() {
-        isConnected.add(1);
-        for (Connection next : conn[1]) {
-            pq.add(next);
+
+        for(Node init : graph[1]){
+            pq.add(init);
         }
 
-        while (!pq.isEmpty()) {
-            Connection next = pq.poll();
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
 
-            if (isConnected.contains(next.next)) {
+            if(connected.contains(cur.src) && connected.contains(cur.dest)){
                 continue;
             }
-            isConnected.add(next.next);
-            distance += next.weight;
 
-            for (Connection x : conn[next.next]) {
-                pq.add(x);
-            }
+            connected.add(cur.src);
+            connected.add(cur.dest);
+            sum += cur.cost;
+
+            // for(Node next : graph[cur.src]){
+            //     if(!connected.contains(next.dest)){
+            //         pq.add(next);
+            //     }
+            // }
+
+            for(Node next : graph[cur.dest]){
+                if(!connected.contains(next.dest)){
+                    pq.add(next);
+                }
+            }            
         }
     }
-    
-    public static void pre() throws IOException {
+
+    public static void inputs() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        distance = 0;
+        sum = 0;
 
-        isConnected = new HashSet<>();
+        graph = new List[N + 1];
         pq = new PriorityQueue<>();
-        conn = new List[N + 1];
-        
+        connected = new HashSet<>();
+
         for (int i = 1; i <= N; i++) {
-            conn[i] = new ArrayList<>();
+            graph[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int V = Integer.parseInt(st.nextToken());
-            int U = Integer.parseInt(st.nextToken());
-            int W = Integer.parseInt(st.nextToken());
-            conn[V].add(new Connection(U, W));
-            conn[U].add(new Connection(V, W));
+
+            int inputA = Integer.parseInt(st.nextToken());
+            int inputB = Integer.parseInt(st.nextToken());
+            int inputW = Integer.parseInt(st.nextToken());
+
+            graph[inputA].add(new Node(inputA, inputB, inputW));
+            graph[inputB].add(new Node(inputB, inputA, inputW));
         }
     }
 
     public static void main(String args[]) throws IOException {
-        pre();
+        inputs();
         Prims();
-        System.out.println(distance);
+        System.out.println(sum);
     }
 
-    static class Connection implements Comparable<Connection> {
-        int next, weight;
+    static class Node implements Comparable<Node> {
+        int src, dest, cost;
 
-        public Connection() {
-        }
-
-        public Connection(int next, int weight) {
-            this.next = next;
-            this.weight = weight;
+        public Node(int src, int dest, int cost) {
+            this.src = src;
+            this.dest = dest;
+            this.cost = cost;
         }
 
         @Override
-        public int compareTo(Connection o) {
-            return this.weight - o.weight;
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
         }
     }
 }
