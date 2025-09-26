9d1ea9f302c83a708e829d0b6b57e98e97dcde09
Changyeong Yeo <67392167+duckddud213@users.noreply.github.com>
2025-09-27 02:02:59 +0900

    [Gold IV] Title: 최단경로, Time: 652 ms, Memory: 108868 KB -BaekjoonHub

--- Java/백준/Gold/1753. 최단경로/최단경로.java
+++ Java/백준/Gold/1753. 최단경로/최단경로.java
@@ -3,81 +3,78 @@ import java.util.*;
 
 public class Main {
     static int V, E, K;
-    static boolean isVisited[];
     static int distance[];
-    static List<Node>[] graph;
-    static PriorityQueue<Node> que;
+    static boolean isChecked[];
+    static List<Node> line[];
     static StringBuilder sb;
 
     public static void dijkstra() {
-        que.add(new Node(K, 0));
-        distance[K] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
 
-        while (!que.isEmpty()) {
-            Node cur = que.poll();
+        pq.add(new Node(K, 0));
 
-            isVisited[cur.num] = true;
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
 
-            for (Node next : graph[cur.num]) {
-                if (!isVisited[next.num] && distance[next.num] > cur.dist + next.dist) {
+            isChecked[cur.num] = true;
+
+            for (Node next : line[cur.num]) {
+                if (!isChecked[next.num] && distance[next.num] > cur.dist + next.dist) {
                     distance[next.num] = cur.dist + next.dist;
-                    que.add(new Node(next.num, distance[next.num]));
+                    pq.add(new Node(next.num, distance[next.num]));
                 }
+            }
+        }
+
+        for(int i = 1; i <= V;i++){
+            if(i == K){
+                sb.append("0").append("\n");
+            }
+            else if(distance[i] == Integer.MAX_VALUE){
+                sb.append("INF").append("\n");
+            }
+            else{
+                sb.append(distance[i]).append("\n");
             }
         }
     }
 
-    public static void pre() throws IOException {
-        int i;
+    public static void inputs() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        que = new PriorityQueue<>();
-        sb = new StringBuilder();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(br.readLine());
 
+        line = new List[V + 1];
         distance = new int[V + 1];
-        isVisited = new boolean[V + 1];
-        graph = new ArrayList[V+1];
+        isChecked = new boolean[V + 1];
+        sb = new StringBuilder();
 
-        for (i = 1; i <= V; i++) {
-            graph[i] = new ArrayList<>();
+        for (int i = 1; i <= V; i++) {
+            line[i] = new ArrayList<>();
             distance[i] = Integer.MAX_VALUE;
         }
 
-        for (i = 0; i < E; i++) {
+        for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-            int inputU = Integer.parseInt(st.nextToken());
-            int inputV = Integer.parseInt(st.nextToken());
-            int inputW = Integer.parseInt(st.nextToken());
 
-            graph[inputU].add(new Node(inputV, inputW));
+            int inputV = Integer.parseInt(st.nextToken());
+            int inputE = Integer.parseInt(st.nextToken());
+            int inputK = Integer.parseInt(st.nextToken());
+
+            line[inputV].add(new Node(inputE, inputK));
         }
     }
 
     public static void main(String args[]) throws IOException {
-        pre();
+        inputs();
         dijkstra();
-        for (int i = 1; i <= V; i++) {
-            if (distance[i] == Integer.MAX_VALUE) {
-                sb.append("INF").append('\n');
-            } 
-            else {
-                sb.append(distance[i]).append('\n');
-            }
-        }
         System.out.print(sb);
     }
-    
-    static class Node implements Comparable<Node>{
-        int num, dist;
 
-        public Node() {
-        }
+    static class Node implements Comparable<Node> {
+        int num, dist;
 
         public Node(int num, int dist) {
             this.num = num;
