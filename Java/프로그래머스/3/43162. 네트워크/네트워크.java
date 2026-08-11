6cef1d3440596b1bdb252d66a03534cd1ce1ef27
Changyeong Yeo <67392167+duckddud213@users.noreply.github.com>
2026-08-11 17:05:44 +0900

    [level 3] Title: 네트워크, Time: 7.06 ms, Memory: 82 MB -BaekjoonHub

--- Java/프로그래머스/3/43162. 네트워크/네트워크.java
+++ Java/프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,69 +2,34 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    static int answer, size;
-    static int dir[][] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    static HashSet<Integer> isChecked;
+    static int answer;
+    static boolean isVisited[];
     static PriorityQueue<Integer> pq;
-    
-    public void bfs(int com[][]){
-        for(int i = 0; i < size; i++){
-            if(isChecked.contains(i)){
+    public int solution(int n, int[][] computers) {
+        answer = 0;
+        isVisited = new boolean[n];
+        
+        for(int i = 0; i < n; i++){
+            if(isVisited[i]){
                 continue;
             }
             
-            isChecked.add(i);
             answer++;
-            
-            for(int j = 0; j < size; j++){
-                if(com[i][j] == 1 && !isChecked.contains(j)){
-                    pq.add(j);
-                }
-            }
+            pq = new PriorityQueue<>();
+            pq.add(i);
             
             while(!pq.isEmpty()){
-                int next = pq.poll();
-                isChecked.add(next);
-                for(int j = 0; j < size; j++){
-                    if(!isChecked.contains(j) && com[next][j] == 1){
+                int num = pq.poll();
+                isVisited[num] = true;
+                
+                for(int j = 0; j < n; j++){
+                    if(computers[num][j] == 1 && !isVisited[j]){
                         pq.add(j);
                     }
                 }
             }
-            
-            pq.clear();
         }
-    }
-    
-    public boolean isValid(int x, int y){
-        return x >= 0 && x < size && y >= 0 && y < size;
-    }
-    
-    public int solution(int n, int[][] computers) {
-        answer = 0;
-        size = n;
-        isChecked = new HashSet<>();
-        pq = new PriorityQueue<>();
-        
-        bfs(computers);
         
         return answer;
-    }
-    
-    static class Pos implements Comparable<Pos>{
-        int i,j;
-        
-        public Pos(int i, int j){
-            this.i = i;
-            this.j = j;
-        }
-        
-        @Override
-        public int compareTo(Pos o){
-            if(this.i == o.i){
-                return this.j - o.j;
-            }
-            return this.i - o.i;
-        }
     }
 }
